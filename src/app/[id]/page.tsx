4f08f7fab7a7abe746c9f86adc2d81df2715b418
FaroukBentaleb <farouk.bentaleb@esprit.tn>
2025-08-02 15:45:08 +0100

    Changed

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -7,20 +7,22 @@ import { JsonData } from '@prisma/client';
 import { useParams } from 'next/navigation';
 
 export default function SharedJson() {
-  const { id } = useParams<{ id: string }>();
-  const [jsonData, setJsonData] = useState<JsonData>();
+  const { id } = useParams() as { id: string };
+  const [jsonData, setJsonData] = useState<JsonData | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(`/api/json/${id}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
         const data = await response.json();
-
         setJsonData(data);
-        setLoading(false);
       } catch (error) {
         console.error('Failed to fetch data:', error);
+      } finally {
         setLoading(false);
       }
     };
@@ -29,13 +31,18 @@ export default function SharedJson() {
   }, [id]);
 
   if (loading) {
-    return <div className='mt-8'>loading...</div>;
+    return <div className='mt-8'>Loading...</div>;
   }
+
+  if (!jsonData) {
+    return <div className='mt-8'>Data not found</div>;
+  }
+
   return (
     <div className='mt-8 space-y-4'>
-      <h1 className='text-2xl underline font-bold'>{jsonData?.name}</h1>
+      <h1 className='text-2xl underline font-bold'>{jsonData.name}</h1>
       <CodeMirror
-        value={jsonData?.content || ''}
+        value={jsonData.content || ''}
         height='400px'
         extensions={[json()]}
         editable={false}
--- src/app/api/json/[id]/route.ts
+++ src/app/api/json/[id]/route.ts
@@ -1,8 +1,8 @@
 import prisma from "@/lib/prisma";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function GET(
-  request: NextRequest,
+  request: Request,
   { params }: { params: { id: string } }
 ) {
   const { id } = params;
